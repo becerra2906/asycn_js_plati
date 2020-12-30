861d66f5fcf8e495265f30b4d2104bac80fb530d
Alejandro <alejobecerra1995@gmail.com>
2020-12-29 23:08:17 -0500

    added specifications of the exercise (consume Rick and Morty api and get certain data) and created doc for class 3

---
+++ clase2.js
@@ -0,0 +1,11 @@
+// primer ejecicio: consumir api y sacar lista completa de personajes
+
+// 2 sacar el nombre del primer personaje
+// 3 sacar la dimensión a donde pertenece ese personaje. 
+
+// api link https://rickandmortyapi.com/
+
+// api docs https://rickandmortyapi.com/documentation
+
+
+
---
+++ clase3_callbacks.js
@@ -0,0 +1,1 @@
+// estructura de un callback
